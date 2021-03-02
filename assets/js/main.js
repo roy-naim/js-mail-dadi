c448aa6f41435d784d4b841abce76e30b022bf20
Roy <b.roynaim@gmail.com>
2021-03-02 17:45:36 +0100

    Mail and ciclo completed

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var mail = prompt("What's your mail?");
+console.log(mail);
+
+var names = ["Roy", "Leon", "Tommaso", "David"]
+
+for (var i = 0; i < names.length; i++) {
+  console.log(names[i])
+}
