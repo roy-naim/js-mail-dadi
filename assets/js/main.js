ebda136d6e7c86924902c213977715fed88562be
Roy <b.roynaim@gmail.com>
2021-03-03 09:48:39 +0100

    Esercizio mail basic completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,15 @@
 var emails = ["roy@mail.com", "elena@mail.com", "joe@mail.com"]
 
 var my_email = prompt("What's your mail?");
+var flag = false;
 for (var i = 0; i < emails.length; i++) {
-  if (my_email[i]) {
-    console.log(emails[i])
+  if  (emails[i] == my_email) {
+    flag = true
   }
+}
+//flag is something that u can stop the loop after u've writtern ur mail
+if (flag) {
+  console.log("Your mail is here.")
+}else {
+  console.log("Sorry, you're not subscribed yet.")
 }
